9b179a67de8b98e26c0a1f7f5a830a2f72112bf8
seventyfire <xwg1997@outlook.com>
2021-03-11 16:33:56 +0800

    gateway:add script template

--- gateway.py
+++ gateway.py
@@ -6,10 +6,38 @@ class Gateway:
         self.server_ip = server_ip
         self.server_port = server_port
 
-
     def get_reverse_proxy_config_info(self):
-        return "server{listen " + str(self.server_port) + ";server_name _;location / {proxy_pass http://" + self.vpn_ip + ":" + str(self.vpn_port) + ";}}"
+        return "server{listen " + str(
+            self.server_port) + ";server_name _;location / {proxy_pass http://" + self.vpn_ip + ":" + str(
+            self.vpn_port) + ";}}"
 
+    def get_script_template(self):
+        return '''
+import subprocess
+import os
+
+def get_client_config_info():
+    return 'pty "pptp smrtyan.cn --nolaunchpppd" name culiu remotename PPTP require-mppe-128 file /etc/ppp/options.pptp ipparam culiu'
+
+
+device_IMEI = subprocess.check_output(['usi get cellular.status[1].imei']).decode('utf-8')
+if device_IMEI == ''' + self.IMEI + ''':
+    if os.system("apt-get install pptp-linux"):
+        os.system("echo 'installation error'")
+        exit()
+    secrets_file = open("/etc/ppp/chap-secrets", "a")
+    secrets_file.write("culiu	PPTP	culiu	''' + self.vpn_ip + ''')
+    secrets_file.close()
+    client_config_file = open("/etc/ppp/peers/culiu", "w")
+    client_config_file.write(get_client_config_info())
+    client_config_file.close()
+    if os.system("pon culiu"):
+        os.system("echo 'pptp service start error'")
+        exit()
+    os.system("echo 'pptp service started'")
+else:
+    os.system("echo 'device imei number is not matched'")
+        '''
 
     def add(self):
         import os
@@ -24,11 +52,32 @@ class Gateway:
         reverse_proxy_config_file.write(self.get_reverse_proxy_config_info())
         reverse_proxy_config_file.close()
 
-        os.system("nginx -s reload")
-        os.system("echo 'reloaded nginx'")
+        if os.system("nginx -s reload"):
+            return "error"
+        if os.system("echo 'reloaded nginx'"):
+            return "error"
         # step3. create a new bash script file and send it back to caller
         # script file: use to install vpn app on gateway that have the right IMEI
         # and to config some needed information.
+        script_file = open("./script/client_config.py", "w")
+        script_file.write(self.get_script_template())
+
+
+
+
+
+# apt-get install pptp-linux  -> installation
+
+# # /etc/ppp/peers/culiu   -> config file
+# pty "pptp smrtyan.cn --nolaunchpppd"
+# name culiu
+# remotename PPTP
+# require-mppe-128
+# file /etc/ppp/options.pptp
+# ipparam culiu
+
+# pon culiu -> command
+
 
 
 if __name__ == "__main__":
