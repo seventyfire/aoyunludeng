bb2d81276cb3fe539265d41e2485b09ab194db55
seventyfire <xwg1997@outlook.com>
2021-03-15 19:11:56 +0800

    running...

--- script/client_config.py
+++ script/client_config.py
@@ -14,8 +14,7 @@ if device_IMEI == "867698045127654":
         os.system("echo 'installation error'")
         exit()
     secrets_file = open("/etc/ppp/chap-secrets", "a")
-    secrets_file.write("culiu1	PPTP	culiu1	172.16.0.102
-")
+    secrets_file.write("culiu1	PPTP	culiu1	172.16.0.102\n")
     secrets_file.close()
     client_config_file = open("/etc/ppp/peers/culiu", "w")
     client_config_file.write(get_client_config_info())
